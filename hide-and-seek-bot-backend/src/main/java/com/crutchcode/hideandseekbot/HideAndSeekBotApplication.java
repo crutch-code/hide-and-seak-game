6715f7ae2210984cc725146ce0aab958df48898e
Boris <boris.stupin.01@gmail.com>
2022-06-24 13:21:55 +0300

    initial commit

---
+++ hide-and-seek-bot-backend/src/main/java/com/crutchcode/hideandseekbot/HideAndSeekBotApplication.java
@@ -0,0 +1,13 @@
+package com.crutchcode.hideandseekbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HideAndSeekBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HideAndSeekBotApplication.class, args);
+    }
+
+}
---
+++ hide-and-seek-bot-backend/src/test/java/com/crutchcode/hideandseekbot/HideAndSeekBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crutchcode.hideandseekbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HideAndSeekBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
